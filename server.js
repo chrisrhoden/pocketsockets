5b9bc6269a8d56e9ce8cdf2078b2636715427781
chrisrhoden <carhoden@gmail.com>
2012-04-07 11:14:44 -0400

    views

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var express = require('express');
+var express = require('express')
+  , sockets = require('socket.io');
 
 var app = express.createServer();
+var io  = sockets.listen(app);
 
 app.configure(function() {
   app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 })
 
 app.get('/', function(req, res) {
-  res.end("SUCCESS!");
+  res.render('index.jade', {title:"Hi"});
 });
 
 app.get('/mobile', function(req, res) {
+  res.end("SUCCESS!");
+});
+
+io.sockets.on('connection' , function (socket) {
 
 });
 
